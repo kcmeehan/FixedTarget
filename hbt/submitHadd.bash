167ce454bf501bfadb42a3e81251eb9c3ba84613
John Campbell <johnmarcampbell@gmail.com>
2018-04-25 14:34:37 -0400

    Change hadd script behavior for low pT analysis

--- hbt/submitHadd.bash
+++ hbt/submitHadd.bash
@@ -2,7 +2,8 @@
 
 scratchRoot=/gpfs/mnt/gpfs01/star/scratch/$USER/fxtHbt
 
+star-submit-template -template submitHadd.xml -entities scratchRoot=$scratchRoot,fileGlob=4x5GeVFemto_lowPt
+
 for((i=0; i<=5; i++)); do
-    star-submit-template -template submitHadd.xml -entities scratchRoot=$scratchRoot,fileGlob=4x5GeVFemto_lowPt_multBin_${i}
     star-submit-template -template submitHadd.xml -entities scratchRoot=$scratchRoot,fileGlob=4x5GeVFemto_highPt_multBin_${i}
 done
