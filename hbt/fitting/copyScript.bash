655c6b69f3856694b9e670566eac2520d0cad2a4
John Campbell <johnmarcampbell@gmail.com>
2018-04-25 12:30:19 -0400

    Change copyScript.bash to copy different files

--- hbt/fitting/copyScript.bash
+++ hbt/fitting/copyScript.bash
@@ -1,20 +1,25 @@
 #! /bin/bash
 
+# Run this script using:
+#    copyScript.bash [rcfName] [rcfDirectory]
+# where [rcfName] is your user name on RCF and [rcfDirectory] is the directory
+# on RCF where the output histograms are stored, most likely somewhere in your
+# scratch space.
+#
+# This script will copy the files to the output_histograms directory.
+
+
 rcfName=$1
 rcfDirectory=/star/u/$1/$2
 rcfUrl=rftpexp.rhic.bnl.gov
 
-# Copy the output files for each multiplicity bin
-# AND convert from multiplicity bin to centrality bin
+# Copy the output files for each multiplicity bin of the high-pt analysis
 for((mult=0; mult<=5; mult++))
 do
-    cent=$((6-$mult))
-
-    sourceFilename=4x5GeVFemto_lowPt_multBin_$mult.root
-    destinationFilename=4x5GeVFemto_lowPt_cent_$cent.root
-    scp $rcfName@$rcfUrl:$rcfDirectory/$sourceFilename output_histograms/$destinationFilename
-
-    sourceFilename=4x5GeVFemto_highPt_multBin_$mult.root
-    destinationFilename=4x5GeVFemto_highPt_cent_$cent.root
-    scp $rcfName@$rcfUrl:$rcfDirectory/$sourceFilename output_histograms/$destinationFilename
+    fileName=4x5GeVFemto_highPt_multBin_$mult.root
+    scp $rcfName@$rcfUrl:$rcfDirectory/$fileName output_histograms/$fileName
 done
+
+# Copy the output files for the low-pt analysis
+fileName=4x5GeVFemto_lowPt.root
+scp $rcfName@$rcfUrl:$rcfDirectory/$fileName output_histograms/$fileName
