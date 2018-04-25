deeea42d3e9f08a25dc827e01982ed2957ff64fd
John Campbell <johnmarcampbell@gmail.com>
2018-04-25 14:39:25 -0400

    Clean up submit script

--- hbt/submitFxtFemtoAnalysis.bash
+++ hbt/submitFxtFemtoAnalysis.bash
@@ -2,15 +2,14 @@
 lowPtTag=4x5GeVFemto_lowPt
 highPtTag=4x5GeVFemto_highPt
 scratchRoot=/gpfs/mnt/gpfs01/star/scratch/$USER/fxtHbt
-# tag=4x5GeVFemto_widePt_excludeHighMult
 
 # Loop over multiplicity bins for low pt analysis
-# for((i=4; i<=5; i++))
-# do
-#     star-submit-template -template submitFxtFemtoAnalysis.xml -entities multBin=$i,fileTag=$lowPtTag,minPt=0.1,maxPt=0.3,scratchRoot=$scratchRoot
-# done
+for((i=4; i<=5; i++))
+do
+    star-submit-template -template submitFxtFemtoAnalysis.xml -entities multBin=$i,fileTag=$lowPtTag,minPt=0.1,maxPt=0.3,scratchRoot=$scratchRoot
+done
 
-Loop over multiplicity bins for high pt analysis
+# Loop over multiplicity bins for high pt analysis
 for((i=0; i<=5; i++))
 do
     star-submit-template -template submitFxtFemtoAnalysis.xml -entities multBin=$i,fileTag=$highPtTag,minPt=0.15,maxPt=0.8,scratchRoot=$scratchRoot
